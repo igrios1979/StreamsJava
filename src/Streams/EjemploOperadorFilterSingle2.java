a05b8a4519fc09354eb73c347268f7bf3ba2fdf5
igrios <ignarios1@hotmail.com>
2023-03-09 16:21:51 -0300

    operadores terminales solo debe haber un uno por ejemplo foreach, Anymatch,findfirst

--- src/Streams/EjemploOperadorFilterSingle2.java
+++ src/Streams/EjemploOperadorFilterSingle2.java
@@ -9,16 +9,15 @@ public class EjemploOperadorFilterSingle2 {
 
     public static void main(String[] args) {
 
-        Usuario usuario = Stream
+        boolean existe = Stream
                 .of("Pato fontanet","Paco Delucia","lucho ibañez","lucho gatica","ignacio Rios","Elton john")
                 .map(nombre -> new Usuario(nombre.split(" ")[0],nombre.split(" ")[1],0))
                 .peek(System.out::println)
-                .filter(z->z.getId().equals(6))
-                .findFirst().get(); // operador terminal cuando lo encuentra deja de buscar
+                .anyMatch(u->u.getId().equals(7));
 
 
 
-        System.out.println("usuario nro  "+usuario.getId()+ " " + usuario);
+        System.out.println(existe);
 
 
 
